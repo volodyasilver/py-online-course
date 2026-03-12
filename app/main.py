67ad922006ee06ec3a24731eeb5a11416499f842
Volodymyr Stashenko <volodyasilver@gmail.com>
2026-03-12 18:23:21 +0200

    Implement OnlineCourse class with initialization and methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
+import math
+from typing import Dict, Any, Type
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return math.ceil(days / 7)
+
+    @classmethod
+    def from_dict(cls: Type["OnlineCourse"], course_dict: Dict[str, Any]) -> "OnlineCourse":
+
+        weeks = cls.days_to_weeks(course_dict["days"])
+        
+        return cls(
+            name=course_dict["name"],
+            description=course_dict["description"],
+            weeks=weeks
+        )
